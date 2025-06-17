31061398be2aa0faf9be4bc3c60ee058343862cc
gruszm <mariuszgruszczynski11@gmail.com>
2025-06-17 14:59:56 +0200

    Added entity for user's data

---
+++ src/main/java/pl/gruszm/profile_service/entities/UserData.java
@@ -0,0 +1,81 @@
+package pl.gruszm.profile_service.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "user_data")
+public class UserData
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "First name is required.")
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Last name is required.")
+    private String lastName;
+
+    @Column(name = "phone_number", nullable = false)
+    @NotBlank(message = "Phone number is required.")
+    @Pattern(regexp = "^(\\+\\d{2,3}[\\s-]?)?(\\d{3}[\\s-]?\\d{3}[\\s-]?\\d{3})$")
+    private String phoneNumber;
+
+    public UserData()
+    {
+
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName()
+    {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName)
+    {
+        this.middleName = middleName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+}
