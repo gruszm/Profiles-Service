2e8dee66aa3aa8639cb5a9aa7fe72b9bd52b20d8
gruszm <mariuszgruszczynski11@gmail.com>
2025-08-15 21:41:48 +0200

    Endpoint: get addresses by user header

--- src/main/java/pl/gruszm/profiles_service/controllers/AddressSecureController.java
+++ src/main/java/pl/gruszm/profiles_service/controllers/AddressSecureController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.gruszm.profiles_service.DTOs.AddressDTO;
 import pl.gruszm.profiles_service.DTOs.UserHeader;
+import pl.gruszm.profiles_service.entities.Address;
 import pl.gruszm.profiles_service.services.AddressService;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,6 +24,45 @@ public class AddressSecureController
 
     @Autowired
     private AddressService addressService;
+
+    @GetMapping
+    public ResponseEntity<?> getAddresses(@RequestHeader("X-User") String userHeaderJson)
+    {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        try
+        {
+            UserHeader userHeader = objectMapper.readValue(userHeaderJson, UserHeader.class);
+            List<Address> addresses = addressService.getAddressesByUserId(userHeader.getId());
+
+            if (addresses == null || addresses.isEmpty())
+            {
+                return ResponseEntity.notFound().build();
+            }
+            else
+            {
+                return ResponseEntity.ok(addresses);
+            }
+        }
+        catch (JsonProcessingException e)
+        {
+            errorResponse.put("message", e.getMessage());
+
+            return ResponseEntity.internalServerError().body(errorResponse);
+        }
+        catch (IllegalArgumentException e)
+        {
+            errorResponse.put("message", e.getMessage());
+
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
+        catch (Exception e)
+        {
+            errorResponse.put("message", "Internal server error");
+
+            return ResponseEntity.internalServerError().body(errorResponse);
+        }
+    }
 
     @PostMapping
     public ResponseEntity<?> addAddressForUser(@RequestHeader("X-User") String userHeaderJson, @RequestBody AddressDTO addressDTO)
